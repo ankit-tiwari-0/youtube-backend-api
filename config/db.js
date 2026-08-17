ea3b36fcacf233885ff7ce93b2b490a014309824
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-08-17 12:20:35 +0530

    add mongoose and add to database

---
+++ config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI)
+    } catch (error) {
+        console.log((error.message));
+        
+        throw new Error("Something went wrong")
+    }
+}
