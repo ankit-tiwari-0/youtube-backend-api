8572664d4f7bd384f34aec6c9ff15a3481f56e31
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-08-17 12:23:09 +0530

    ✅

--- config/db.js
+++ config/db.js
@@ -4,6 +4,8 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI)
+        console.log("db is connected✅");
+        
     } catch (error) {
         console.log((error.message));
         
