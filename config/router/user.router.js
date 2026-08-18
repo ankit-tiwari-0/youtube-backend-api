fafc8e86b6521e2ef1e9ce7c39ec0a2c3a8d2f72
ankit‑tiwari‑0 <ankit.tiwari.47xxx@gmail.com>
2026-08-18 19:46:00 +0530

    lets start work on login

--- config/router/user.router.js
+++ config/router/user.router.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { signup } from "../../controller/user.controller.js";
+import { login, signup } from "../../controller/user.controller.js";
 
 const useroute = Router()
 
 useroute.post("/signup",signup )
+useroute.post("/login", login)
 
 
 
--- controller/user.controller.js
+++ controller/user.controller.js
@@ -41,3 +41,7 @@ export const signup = async (req, res) => {
         });
     }
 }
+
+export const login = async(req, res) =>{
+    res.send("ok")
+}
